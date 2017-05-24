4d712f50f1d21680be54de8f51a5ae0936cc00e1
SaadAfzal <saadafzal89@gmail.com>
2017-05-24 18:48:38 -0400

    testing

---
+++ Chapter_3/ctci_3_5/SortedStack.java
@@ -0,0 +1,17 @@
+public class SortedStack{
+    public static void main(String[] args){
+
+    }
+}
+
+class MyStack{
+
+    int index =-1;
+    int arr[] = new int[5];
+
+    void push(int item){
+        if(index<arr.length){
+
+        }
+    }
+}
